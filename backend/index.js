a0d686d05aa7c36c29ccbfff618a71a634189daf
PIERRE AHSATAL <asathal.pierre@gmail.com>
2022-08-08 16:22:54 +0200

    fix config env

--- backend/index.js
+++ backend/index.js
@@ -14,10 +14,10 @@ const port = process.env.PORT || 5000;
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: "eu-cdbr-west-03.cleardb.net",
-  user: "b18a40e6c9209e",
-  password: "2873b2c1",
-  database: "heroku_e403c996856297a",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 /* const db = connection.promise();  */
@@ -73,91 +73,6 @@ app.get("/", (req, res) => {
     }
   });
 });
-
-// RETRIEVE ONE PROJECT
-/* app.get("/project/:id", (req, res) => {
-  const projectId = req.params.id;
-  connection.query(
-    "SELECT * FROM project WHERE id = ?",
-    [projectId],
-    (err, results) => {
-      if (err) {
-        res.status(500).send("Error retrieving project from database");
-      } else if (results.length) res.json(results[0]);
-      else res.status(404).send("project not found");
-    }
-  );
-}); */
-
-// POST PROJECT
-/* app.post("/project", (req, res) => {
-  const { title, texte } = req.body;
-  const { error } = Joi.object({
-    title: Joi.string().max(255).required(),
-    texte: Joi.string().max(10000).required(),
-  }).validate({ title, texte }, { abortEarly: false });
-  if (error) {
-    res.status(422).json({ validationErrors: error.details });
-  } else {
-    connection.query(
-      "INSERT INTO project (title, content, techno) VALUES (?, ?, ?)",
-      [title, texte],
-      (err, result) => {
-        if (err) {
-          res.status(500).send("Error saving the project");
-        } else {
-          const id = result.insertId;
-          const createdProject = { id, title, texte };
-          res.status(201).json(createdProject);
-        }
-      }
-    );
-  }
-}); */
-
-// UPDATE PROJECT
-/* app.put("/project/:id", (req, res) => {
-  const projectId = req.params.id;
-  const datbase = connection.promise();
-  let existingProject = null;
-  datbase
-    .query("SELECT * FROM project WHERE id = ?", [projectId])
-    .then(([results]) => {
-      // eslint-disable-next-line prefer-destructuring
-      existingProject = results[0];
-      // eslint-disable-next-line prefer-promise-reject-errors
-      if (!existingProject) return Promise.reject("RECORD_NOT_FOUND");
-      return datbase.query("UPDATE project SET ? WHERE id = ?", [
-        req.body,
-        projectId,
-      ]);
-    })
-    .then(() => {
-      res.status(200).json({ ...existingProject, ...req.body });
-    })
-    .catch((err) => {
-      console.error(err);
-      if (err === "RECORD_NOT_FOUND")
-        res.status(404).send(`project with id ${projectId} not found.`);
-      else res.status(500).send("Error updating a project.");
-    });
-}); */
-
-// DELETE PROJECT
-/* app.delete("/project/:id", (req, res) => {
-  const projectId = req.params.id;
-  connection.query(
-    "DELETE FROM project WHERE id = ?",
-    [projectId],
-    (err, result) => {
-      if (err) {
-        res.status(500).send("Error deleting a project");
-      } else if (result.affectedRows)
-        res.status(200).send("🎉 project deleted!");
-      else res.status(404).send("project not found");
-    }
-  );
-}); */
 
 app.listen(process.env.PORT || port, (error) => {
   connection.connect((err) => {
